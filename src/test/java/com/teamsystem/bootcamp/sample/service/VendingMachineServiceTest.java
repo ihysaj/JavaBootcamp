74f4ae03b0e29ce33b2d8c918ec510e1a47bf805
Gianluca Genga <gianluca.genga@mondora.com>
2022-06-21 10:00:52 +0200

    mock test

--- src/test/java/com/teamsystem/bootcamp/sample/service/VendingMachineServiceTest.java
+++ src/test/java/com/teamsystem/bootcamp/sample/service/VendingMachineServiceTest.java
@@ -1,9 +1,14 @@
 package com.teamsystem.bootcamp.sample.service;
 
 import com.teamsystem.MainApplication;
+import com.teamsystem.bootcamp.sample.entity.Coin;
+import com.teamsystem.bootcamp.sample.repository.CoinRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 //import org.junit.runner.RunWith;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,16 +16,32 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 //@RunWith(SpringRunner.class)
 @SpringBootTest(classes = {MainApplication.class})
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class VendingMachineServiceTest {
 
-    @Autowired
+ //   @Autowired
     private VendingMachineService vendingMachineService;
+
+    @Mock
+    private CoinRepository coinRepository;
+
+    @BeforeAll
+    public void setup() {
+        when(coinRepository.getCountryWithMaxNumberOfCoins()).thenReturn("FR");
+        when(coinRepository.countAllByCountryId(any())).thenReturn(10);
+        when(coinRepository.countAllByCountryId(any())).thenReturn(5);
+        vendingMachineService = new VendingMachineService(coinRepository);
+    }
+
     @Test
     public void test() {
-        System.out.println(vendingMachineService.getAllByCountry("IT"));
-        System.out.println(vendingMachineService.getAllByCountry("AL"));
+  //      System.out.println(vendingMachineService.getAllByCountry("IT"));
+  //      System.out.println(vendingMachineService.getAllByCountry("AL"));
         System.out.println(vendingMachineService.countAllByCountry("IT"));
         System.out.println(vendingMachineService.countAllByCountry("AL"));
 
